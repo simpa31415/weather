cdb373e2acdb39601319824eb5fb35836006d931
simpa31415 <simon.astrand@gmail.com>
2026-02-20 13:26:18 +0100

    main

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def home():
                            prec_prob=prec_prob,
                            wind_max=wind_max)
 
-if __name__ == "__master__":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
